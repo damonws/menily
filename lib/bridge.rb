0f270d5403ef705fd6bb4cb0d2625df33cf35b81
damonws <damonws@82976c65-7328-0410-9df3-efdd9fa7dcad>
2007-03-10 05:59:17 +0000

    Display hands in newspaper bridge format, and include point count.

--- lib/bridge.rb
+++ lib/bridge.rb
@@ -54,6 +54,10 @@ class BridgeHand < Cards
   def lenp
     Suit.to_a.inject(0) { |pts,suit| pts + [0, suit(suit).length - 4].max } 
   end
+
+  def points
+    hcp + lenp
+  end
 end
 
 class BridgeDeal
@@ -87,6 +91,33 @@ class BridgeDeal
   end
 
   def to_s
-    (NORTH..WEST).inject('') { |s,n| "#{s}#{NAME[n]}\n#{@hands[n]}\n" }.strip
+    #(NORTH..WEST).inject('') { |s,n| "#{s}#{NAME[n]}\n#{@hands[n]}\n" }.strip
+    str = Suit.to_a.inject(' '*20 + "#{NAME[NORTH]}\n") do |s,suit|
+      s + hands[NORTH].suit(suit).inject(' '*20 + "#{suit} ") do |ss,card|
+        ss + " #{card.rank}"
+      end + "\n"
+    end
+    str += ' '*20 + "#{hands[NORTH].points} points\n"
+
+    str += NAME[WEST] + ' '*(40-NAME[WEST].length) + NAME[EAST] + "\n"
+    str += Suit.to_a.inject('') do |s,suit|
+      w = hands[WEST].suit(suit).inject("#{suit} ") do |ss,card|
+        ss + " #{card.rank}"
+      end
+      e = hands[EAST].suit(suit).inject("#{suit} ") do |ss,card|
+        ss + " #{card.rank}"
+      end
+      s + w + ' '*(40-w.length) + e + "\n"
+    end
+    w = "#{hands[WEST].points} points"
+    e = "#{hands[EAST].points} points"
+    str += w + ' '*(40-w.length) + e + "\n"
+
+    str += Suit.to_a.inject(' '*20 + "#{NAME[SOUTH]}\n") do |s,suit|
+      s + hands[SOUTH].suit(suit).inject(' '*20 + "#{suit} ") do |ss,card|
+        ss + " #{card.rank}"
+      end + "\n"
+    end
+    str += ' '*20 + "#{hands[SOUTH].points} points\n"
   end
 end
